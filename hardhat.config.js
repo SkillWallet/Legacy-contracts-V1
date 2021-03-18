6af3379850cde26f59bd09bffd7c35116d6f5767
admin <admin@admin.com>
2021-03-18 15:58:24 +0100

    hardhat updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -98,7 +98,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
+        version: "0.8.0",
         settings: {
           optimizer: {
             enabled: true,
@@ -107,7 +107,16 @@ module.exports = {
         }
       },
       {
-        version: "0.8.0",
+        version: "0.7.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.6.10",
         settings: {
           optimizer: {
             enabled: true,
